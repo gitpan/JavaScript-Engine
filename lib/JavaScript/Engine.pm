0af6c81b432e445ee72079631b7e4697d05d96fe
Father Chrysostomos <sprout@cpan.org>
2007-01-25 06:30:04 +0000

    Import of SPROUT/JavaScript-Engine-0.01 from CPAN.
    gitpan-cpan-distribution: JavaScript-Engine
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SPROUT/JavaScript-Engine-0.01.tar.gz
    gitpan-cpan-author:       SPROUT
    gitpan-cpan-maturity:     released

---
+++ lib/JavaScript/Engine.pm
@@ -0,0 +1,22 @@
+package JavaScript::Engine;
+
+our $VERSION = '0.01';
+
+=head1 NAME
+
+JavaScript::Engine - Pure-Perl ECMAScript (JavaScript) engine
+
+=head1 DESCRIPTION
+
+This is just a pointer to L<JE>, a pure-Perl JS engine.
+
+"JE" stands for 
+JavaScript::Engine (following the example of PPI). I put this here so that
+one might be able to find JE when typing C<i /javascript/> in the CPAN
+shell.
+
+=head1 AUTHOR
+
+Father Chrysostomos, C<< <sprout at cpan.org> >>
+
+1; # End of JavaScript::Engine
